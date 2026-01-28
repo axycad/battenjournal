c0572ea3d5c341123f03411886bd7bb96d4d3473
axycad <office@axycad.co.uk>
2026-01-28 12:03:11 +0000

    remove next-pwa

--- next.config.js
+++ next.config.js
@@ -1,45 +1,3 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: true,
-  register: true,
-  skipWaiting: true,
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/.*\.neon\.tech/,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'api-cache',
-        expiration: {
-          maxEntries: 50,
-          maxAgeSeconds: 60 * 60, // 1 hour
-        },
-      },
-    },
-    {
-      urlPattern: /\/_next\/static\/.*/,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'static-cache',
-        expiration: {
-          maxEntries: 200,
-          maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
-        },
-      },
-    },
-    {
-      urlPattern: /\/api\/files\/.*/,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'media-cache',
-        expiration: {
-          maxEntries: 100,
-          maxAgeSeconds: 60 * 60 * 24 * 7, // 7 days
-        },
-      },
-    },
-  ],
-})
-
 const withNextIntl = require('next-intl/plugin')('./src/i18n.ts')
 
 /** @type {import('next').NextConfig} */
@@ -59,4 +17,4 @@ const nextConfig = {
   },
 }
 
-module.exports = withNextIntl(withPWA(nextConfig))
+module.exports = withNextIntl(nextConfig)
